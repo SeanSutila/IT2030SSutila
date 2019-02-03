1ba563f43e9f956aa908e186ee5454fd7b5c50ff
GodofBakon <ssutila@yahoo.com>
2019-02-03 14:56:18 -0500

    1

--- Lab02/Lab02/Controllers/HomeController.cs
+++ Lab02/Lab02/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Lab02.Controllers
     {
         public ActionResult Index()
         {
-            return View("Details");
+            return View("Details")
         }
 
         public ActionResult About()
