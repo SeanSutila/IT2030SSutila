b8252647a90f655b18de30f5f8f0dd7d33b85df6
GodofBakon <ssutila@yahoo.com>
2019-02-03 14:37:54 -0500

    pending

--- Lab02/Lab02/Controllers/HomeController.cs
+++ Lab02/Lab02/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Lab02.Controllers
     {
         public ActionResult Index()
         {
-            return View("C:/Users/Sean/Documents/GitHub/IT2030SSutila/Lab02/Lab02/Views/Other/Details.cshtml");
+            return View("Details");
         }
 
         public ActionResult About()
