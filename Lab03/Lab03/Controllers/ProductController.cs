c52e5d39d47b217209b75b9113e8a9b100588446
GodofBakon <ssutila@yahoo.com>
2019-02-06 16:39:26 -0500

    Lab03 done

---
+++ Lab03/Lab03/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed.";
+        }
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+        public string Details(int id)
+        {
+            return "Details displayed for id=" + id;
+        }
+        public string Location(string zip)
+        {
+            return HttpUtility.HtmlEncode("Location display for zip=" + zip);
+        }
+    }
+}
