fcd9be2746bac58825a076d0c3973d86a7559bd7
GodofBakon <ssutila@yahoo.com>
2019-03-09 21:04:46 -0500

    Week 8 Authentication done

---
+++ Week8LogIn/Week8LogIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week8LogIn.Startup))]
+namespace Week8LogIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
